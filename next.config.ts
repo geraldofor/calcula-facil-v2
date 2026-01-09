16fdebe10e39a8870a753892b507f349a942abb5
geraldofor <133990461+geraldofor@users.noreply.github.com>
2026-01-09 01:26:03 +0000

    config: Usa export estático para evitar cache

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Gera HTML estático
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
